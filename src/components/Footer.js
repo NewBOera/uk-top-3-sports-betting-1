d8f01273802abe2c957d21eec864a94b3a513106
unknown <alexadoniev@gmail.com>
2025-01-29 12:19:52 +0200

    image compress and cdn  path replacement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export class Footer extends LitElement {
   render() {
     return html`
       <link rel="stylesheet" href="/public/styles/output.css" />
-      <section class="mx-auto flex justify-center md:justify-start w-[340px] md:w-[774px] xl:w-[1200px]">
+      <section class="mx-auto flex justify-center md:justify-start w-[340px] md:w-[774px] xl:w-[1200px] py-2">
         <div class="flex justify-between items-center gap-5 w-fit">
           <img src="/public/assets/footer/18.png" alt="age aware" class="justify-self-center object-contain h-[37px]" />
           <img src="/public/assets/footer/gambleAware.png" alt="gamble aware" class="justify-self-center object-contain h-[24px]" />
